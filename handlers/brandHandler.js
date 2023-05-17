825fd5c03d811aa1e9dc1cbf7229ba7fcf42a56b
Aleksandar Janevski <janevskialek@gmail.com>
2023-05-17 14:52:04 +0200

    delete and update

--- handlers/brandHandler.js
+++ handlers/brandHandler.js
@@ -1,4 +1,5 @@
 const brand = require('../pkg/brandModel/brandSchema');
+const car = require('../pkg/carModel/carSchema')
 
 exports.create = async (req, res) => {
     try {
@@ -18,3 +19,26 @@ exports.get = async (req, res) => {
         return res.status(500).json({ status: 'fail', message: err });
     }
 };
+exports.update = async (req, res) => {
+    try {
+        const carBrand = await brand.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        })
+        res.status(200).json({ status: 'success', data: { carBrand } });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ status: 'fail', message: err });
+    }
+};
+exports.delete = async (req, res) => {
+    try {
+        const carBrand = await brand.findById(req.params.id)
+        await car.deleteMany({ company: carBrand.name })
+        await brand.findByIdAndDelete(req.params.id)
+        res.status(204).json({ status: 'success' });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ status: 'fail', message: err });
+    }
+}
