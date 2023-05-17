f29d0f342df12968fd53b3d314cbe8bfad6d3f77
Aleksandar Janevski <janevskialek@gmail.com>
2023-05-17 14:36:42 +0200

    second commit

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const carHandler = require('./handlers/carHandler')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
 
 db.init();
 
