aa2cf8b96e13b9d97417944fa1d36e0752302026
Aleksandar Janevski <janevskialek@gmail.com>
2023-05-17 15:05:43 +0200

    update, delete pre testing

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ app.use(express.json());
 db.init();
 
 app.route('/brand').get(brandHandler.get).post(brandHandler.create);
+app.route('/brand:id').patch(brandHandler.update).delete(brandHandler.delete)
 app.route('/car').get(carHandler.get).post(carHandler.create);
+app.route('/car/:id').patch(carHandler.update).delete(carHandler.delete)
 
 app.listen(process.env.PORT, err => {
     if (err) return console.log(err);
--- handlers/carHandler.js
+++ handlers/carHandler.js
@@ -28,3 +28,28 @@ exports.get = async (req, res) => {
         return res.status(500).json({ status: 'fail', message: err });
     }
 };
+exports.update = async (req, res) => {
+    try {
+        const carModel = await car.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        })
+        res.status(200).json({ status: 'success', data: { carModel } });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ status: 'fail', message: err });
+    }
+};
+exports.delete = async (req, res) => {
+    try {
+        const carModel = await car.findById(req.params.id)
+        const carBrand = await brand.findOne({ name: carModel.company })
+        carBrand.models = carBrand.models.filter(element => element !== req.params._id)
+        await carBrand.save()
+        await car.findByIdAndDelete(req.params.id)
+        res.status(204).json({ status: 'success' });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ status: 'fail', message: err });
+    }
+}
